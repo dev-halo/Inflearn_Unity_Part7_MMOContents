4cc5b90e92a4f770f6edebf2397d6a10f124f79b
Park Chanwoo <dev.halo1005@gmail.com>
2021-11-23 00:39:08 +0900

    구조 개선 - Command 패턴

--- Server/Server/Game/Job/JobSerializer.cs
+++ Server/Server/Game/Job/JobSerializer.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ServerCore
+namespace Server.Game
 {
-    public interface IJobQueue
+    public class JobSerializer
     {
-        void Push(Action job);
-    }
-
-    public class JobQueue : IJobQueue
-    {
-        Queue<Action> jobQueue = new Queue<Action>();
+        Queue<IJob> jobQueue = new Queue<IJob>();
         object _lock = new object();
         bool _flush = false;
 
-        public void Push(Action job)
+        public void Push(Action action) { Push(new Job(action)); }
+        public void Push<T1>(Action<T1> action, T1 t1) { Push(new Job<T1>(action, t1)); }
+        public void Push<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2) { Push(new Job<T1, T2>(action, t1, t2)); }
+        public void Push<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) { Push(new Job<T1, T2, T3>(action, t1, t2, t3)); }
+
+        public void Push(IJob job)
         {
             bool flush = false;
 
@@ -33,15 +33,15 @@ namespace ServerCore
         {
             while (true)
             {
-                Action action = Pop();
-                if (action == null)
+                IJob job = Pop();
+                if (job == null)
                     return;
 
-                action.Invoke();
+                job.Execute();
             }
         }
 
-        Action Pop()
+        IJob Pop()
         {
             lock (_lock)
             {
